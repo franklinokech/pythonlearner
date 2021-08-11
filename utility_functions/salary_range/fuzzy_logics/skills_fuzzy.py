b4ef3abf883cea4b3bd9bc366091489a58d63620
Franklin okech <franklinokech@gmail.com>
2021-08-11 14:40:59 +0300

    added skills fuzzy logic

---
+++ utility_functions/salary_range/fuzzy_logics/skills_fuzzy.py
@@ -0,0 +1,19 @@
+from fuzzywuzzy import process
+
+with open('skills.txt', 'r') as f:
+    skills = f.read().split('\n')
+
+
+def get_matches(query, choices, limit=3):
+    """
+    Function takes in a query, choice and limit do a fuzzy logic and return matching results
+    :param query: string to be searched
+    :param choices: list of possible choices
+    :param limit: return match limit
+    :return: list of matches
+    """
+    results = process.extract(query, choices, limit=limit)
+    return results
+
+
+print(get_matches("['Computer skills', 'Communication skills']", skills, limit=3))
